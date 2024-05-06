e23150649c92375688fe0b66c1d3560ead7c7f4e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-06 16:56:57 -0700

    iter

--- src/yourtactics/__init__.py
+++ src/yourtactics/__init__.py
@@ -9,6 +9,9 @@ def main() -> int:
     out = main2.render_template("go/Makefile.j2")
     path = pathlib.Path("dailycould/{{ cookiecutter.project_slug }}/Makefile")
     path.write_text(out)
+
+    out = main2.render_template("go/Makefile2.j2")
     path = pathlib.Path("allnew/{{ cookiecutter.project_slug }}/Makefile")
     path.write_text(out)
+
     return 0
