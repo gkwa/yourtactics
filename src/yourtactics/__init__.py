251a54dbc39525767feabdd6313bbf204ae6f727
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-06 15:42:46 -0700

    iter

--- src/yourtactics/__init__.py
+++ src/yourtactics/__init__.py
@@ -6,6 +6,6 @@ __project_name__ = "yourtactics"
 
 
 def main() -> int:
-    out = main2.render_template("extended.j2")
+    out = main2.render_template("go/Makefile.j2")
     sys.stdout.write(out)
     return 0
